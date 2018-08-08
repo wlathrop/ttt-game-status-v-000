4dea0f8961bd062e18a67b3ada1b23f352fb30e2
William Lathrop <wlathrop10@gmail.com>
2018-08-08 00:58:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,3 +45,17 @@ def draw?(board)
     return false
   end
 end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
